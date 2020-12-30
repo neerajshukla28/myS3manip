794b21b6930c35733f8473b422b87c77a42808d4
neerajshukla28 <neerajshukla28@gmail.com>
2020-12-30 13:57:30 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-aws_profile=('profile1' 'profile2' 'profile3');
+aws_profile=('');
 #if i have multi profiles  otherwise use single profile
 
 #loop AWS profiles
 for i in "${aws_profile[@]}"; do
   echo "${i}"
-  buckets=($(aws --profile "${i}" --region your_region s3 ls s3:// --recursive | awk '{print $3}'))
+  buckets=($(aws --profile "${i}" --region us-west-2 s3 ls s3:// --recursive | awk '{print $3}'))
 
   #loop S3 buckets
   for j in "${buckets[@]}"; do
   echo "${j}"
-  aws --profile "${i}" --region your_region s3 ls s3://"${j}" --recursive --human-readable --summarize | awk END'{print}'
+  aws --profile "${i}" --region us-west-2 s3 ls s3://"${j}" --recursive --human-readable --summarize 
+  #i removed this      | awk END'{print}'
   done
 
 done
